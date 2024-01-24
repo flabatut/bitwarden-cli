88344c9c7dd66f12fc05e0a0ee609522f1e9129b
Franck Labatut <franck.labatut@ubisoft.com>
2024-01-25 00:16:31 +0100

    fix: need to explicit declare github token

---
+++ internal/registry/main.go
@@ -0,0 +1,16 @@
+package registry
+
+// import (
+// 	"dagger.io/dagger"
+// )
+
+// type Registry struct {
+// 	url      string
+// 	username string
+// 	password *dagger.Secret
+// }
+
+// func (r *Registry) GetCredentials() (string, string, string, error) {
+
+// 	return url, username, password, nil
+// }
